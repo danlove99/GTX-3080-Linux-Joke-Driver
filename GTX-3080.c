e99e53b4e347d6767f5905601b5d3993440bb530
dan <danlove99@hotmail.co.uk>
2020-04-24 18:00:33 +0100

    initial commit

---
+++ GTX-3080.c
@@ -0,0 +1,63 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Robert W. Oliver II");
+MODULE_DESCRIPTION("A simple example Linux module.");
+MODULE_VERSION("0.01");
+
+
+int Aopen(struct inode *pinode, struct file *pfile)
+{
+	return 0;
+}
+ssize_t Aread(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+	printk(KERN_ALERT "You now get mad FPS");
+	return 0;
+}
+ssize_t Awrite(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+{
+	printk(KERN_ALERT "You wrote on the graphics card! :(");
+	return length;
+}
+int Aclose(struct inode *pinode, struct file *pfile)
+{
+	return 0;
+}
+
+
+struct file_operations Afile_operations  = {
+	.owner = THIS_MODULE,
+	.open = Aopen,
+	.read = Aread,
+	.write = Awrite,
+	.release = Aclose
+
+};
+
+int Amodule_init(void)
+{
+	printk(KERN_ALERT " inside the %s function\n", __FUNCTION__);
+
+	/* register with the kernel */
+	register_chrdev(240/* Major Number */,
+			"GTX 3080"/* name of the driver */,
+			&Afile_operations/* file operations */);
+	return 0;
+}
+
+
+void Amodule_exit(void)
+{
+	printk(KERN_ALERT "inside the %s function\n", __FUNCTION__);
+/* unregister */
+	unregister_chrdev(240, "GTX 3080");
+}
+
+module_init(Amodule_init);
+module_exit(Amodule_exit);
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# GTX-3080-Linux-Joke-Driver
+
+![image]('')
