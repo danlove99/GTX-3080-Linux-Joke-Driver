e4c6576cdca6d74413b1bc6a3538d5dd96f37a0e
Daniel Love <danlove99@hotmail.co.uk>
2020-06-10 11:36:25 +0000

    Delete .built-in.a.cmd

--- .built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/dan/Documents/gtx-3080-linux-joke-driver/built-in.a := rm -f /home/dan/Documents/gtx-3080-linux-joke-driver/built-in.a; ar cDPrST /home/dan/Documents/gtx-3080-linux-joke-driver/built-in.a 
